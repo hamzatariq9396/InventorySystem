652b6c6b35e75a39817f799c525c35c060bf8b65
hamza <hamzatariq.mehmood@.com>
2023-12-05 09:40:21 +0500

    Changes

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors({
-    origin:'https://inventory-system-frontend-5bk88eog0-hamzatariqs-projects.vercel.app/',
-    optionsSuccessStatus: 200,
-}));
+// app.use(cors({
+//     origin:'https://inventory-system-frontend-5bk88eog0-hamzatariqs-projects.vercel.app/',
+//     optionsSuccessStatus: 200,
+// }));
 
+
+app.use(cors());
 // ROUTE IMPORTS
 // const product = require("./routes/productRoute");
 
